52eb5d30a32a6f9ce30a4c3474d323e0ebdadd76
Alexandre Lac <al3919@ray24.doc.ic.ac.uk>
2020-01-22 11:07:35 +0000

    feat: add question1

--- question1.py
+++ question1.py
@@ -1,2 +1,22 @@
 import numpy as np 
 
+
+attributes = []
+labels = []
+
+def readfile(filename):
+
+    with open(filename) as f:
+        content = f.readlines()
+
+    for line in content:
+        line_list = line.split(",")
+        attributes.append(line_list[0:-1])
+        labels.append(line_list[-1].strip())
+    
+    # print(np.array(attributes))
+    # print(np.array(labels))
+
+    return np.array(attributes), np.array(labels)
+
+# readfile("data/simple1.txt")
