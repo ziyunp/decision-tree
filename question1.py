65d536602ae0ee8e24861283a9c031b85693d252
Zijian_Ou <kenou1996@126.com>
2020-01-26 19:16:23 +0000

    Fix bugs and refine formatting

--- question1.py
+++ question1.py
@@ -29,44 +29,56 @@ def read_file(filename):
 
     data_list = [raw_line.split(',') for raw_line in raw_data]
     # a [data_num X (attribute_num + 1)] list
-    attributes = np.asarray([line[:-2] for line in data_list], int)
-    label = np.asarray([line[-1] for line in data_list])
+    attributes = np.asarray([line[:-1] for line in data_list], int) # int() automatically removes blank spaces
+    label = np.asarray([line[-1].strip() for line in data_list]) # use strip() to remove blank spaces
     # attributes - a [data_num X attribute_num] array
     # label -  a [data_num X 1] array
 
     attributes_max = np.amax(attributes, axis = 0)
     attributes_min = np.amin(attributes, axis = 0)
+    attributes_range = [(attributes_min[i], attributes_max[i]) for i in range(attribute_num)]
+
+    label_freq = count_frequency(label)
 
     print("Number of data is " + str(data_num))
     print("Number of attributs is " + str(attribute_num))
-    print("Range of attributes are ")
-    print(attributes_max)
-    print(attributes_min)
-    print("Range of labels are ")
-    print(count_frequency(np.transpose(label)))
+    print("\nRange of attributes are ")
+    for i in range(attribute_num): 
+        print("Attr {} : {}.".format(i, attributes_range[i]))
+    print("\nRange of labels are ")
+    for key, value in label_freq.items():
+        print("Label '{}' : {}".format(key, value))
 
     return attributes, label
 
 print("\n********** Information of tran_full.txt **********")
 read_file("data/train_full.txt")
+print("***********************************************\n")
 
 print("\n********** Information of tran_sub.txt **********")
 read_file("data/train_sub.txt")
+print("***********************************************\n")
 
 print("\n********** Information of tran_noisy.txt **********")
 read_file("data/train_noisy.txt")
+print("***********************************************\n")
 
 print("\n********** Information of validation.txt **********")
 read_file("data/validation.txt")
+print("***********************************************\n")
 
 print("\n********** Information of simple1.txt **********")
 read_file("data/simple1.txt")
+print("***********************************************\n")
 
 print("\n********** Information of simple2.txt **********")
 read_file("data/simple2.txt")
+print("***********************************************\n")
 
 print("\n********** Information of test.txt **********")
 read_file("data/test.txt")
+print("***********************************************\n")
 
 print("\n********** Information of toy.txt **********")
 read_file("data/toy.txt")
+print("***********************************************\n")
