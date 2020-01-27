ba8598fb833e87e20f2030609b3a17e7bd89f405
Alexandre Lac <al3919@vm-shell1.doc.ic.ac.uk>
2020-01-27 16:26:36 +0000

    feat: add skeleton

---
+++ helpers.py
@@ -0,0 +1,11 @@
+function sortCategory():
+function sortLabel():
+
+
+function entropy(subset):
+    return entropy
+
+function IG(base, subset1, subset2):
+    return IG
+
+function getLabel(ascii):
---
+++ node.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self):
+        self.entropy = 0
+
+class DecisionNode(Node):
+    # splitPoint
+    # subset1
+    # subset2
+
+class LeafNode(Node):
+    # subset
