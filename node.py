d477f619e3cb8b8f606f2aa33c200b2ee1562bea
Zhang, Kai <dekai.zhang19@imperial.ac.uk>
2020-01-27 16:59:25 +0000

    WIP

--- node.py
+++ node.py
@@ -1,11 +1,52 @@
+import question1
+
 class Node:
     def __init__(self):
         self.entropy = 0
 
 class DecisionNode(Node):
+
+    """
+    A decision node
+    
+    Attributes
+    ----------
+    entropy : float
+        Saves entropy for comparison with children entropy
+    
+    Methods
+    -------
+    calcChildEntropy(subset1, subset2)
+
+    """
+
+    def __init__(self, labels):
+        super.init()
+        self.entropy = calcEntropy(labels)
+
     # splitPoint
+    # labels
     # subset1
     # subset2
 
-class LeafNode(Node):
-    # subset
+    def calcEntropy(labels):
+        freq = countFrequency(labels)
+        total = 0
+        for item in freq:
+            total += freq[item] 
+
+        return total
+        
+
+attributes, labels = readFile("data/toy.txt")
+test = calcEntropy(labels)
+print(test)
+
+
+
+
+
+# class LeafNode(Node):
+#     # subset
+
+
