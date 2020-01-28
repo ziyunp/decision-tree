55c9f37286e958c34f67c87e948d416dd8e24ab1
Alexandre Lac <al3919@ray15.doc.ic.ac.uk>
2020-01-28 16:12:13 +0000

    feat: add findBestSplitPoint()

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import numpy as np 
 from question1 import *
-from node import *
+import math
 
 def convertToAscii(label):
     newArray = []
@@ -15,12 +15,27 @@ def mergeArrays(label, attr):
     return np.asarray(mergedArr)
 
 def sortByAttrAndLabel(data, col):
-        # print(col)
         sortedList = sorted(data, key=lambda x:(x[col], x[0]))
         sortedArr = np.asarray(sortedList)
-        # print(sortedArr)
-        # print("xxxxxxx")
         return sortedArr
+
+def calcEntropy(labels):
+    freq = countFrequency(labels)
+    total = 0
+    for item in freq:
+        total += freq[item] 
+
+    entropy = 0
+
+    for item in freq:
+        entropy -= (freq[item] / total) * math.log(freq[item] / total, 2)
+
+    return entropy
+
+def calcIG(data, subLabel1, subLabel2):
+    dataCount = len(subLabel1) + len(subLabel2)
+    childEntropy = (len(subLabel1) / dataCount) * calcEntropy(subLabel1) + (len(subLabel2) / dataCount) * calcEntropy(subLabel2)
+    return calcEntropy(data) - childEntropy
 
 def checkIG(data, attr, splitPoint):
     
@@ -30,14 +45,14 @@ def checkIG(data, attr, splitPoint):
     for row in data:
         subset1.append(row) if (row[attr] < splitPoint) else subset2.append(row)
     subset1, subset2 = np.asarray(subset1), np.asarray(subset2)
-    return calcIG(data, subset1, subset2)
+    return calcIG(data[:,0], subset1[:,0], subset2[:,0])
 
 ### Hardcoded data ###
 label, attributes = readFile("data/toy.txt")
 ######################
 
 def findBestSplitPoint(data):
-    # bestIG = checkIG(data, 0, 1)
+    bestIG = 0
     bestSplitPoint = None
     dataSet = mergeArrays(convertToAscii(label), attributes)
 
@@ -47,12 +62,9 @@ def findBestSplitPoint(data):
         print(sortedArr)
 
         # find split points
-        # prevClass = sortedArr[0][0]
         prevSplitPoint = sortedArr[0][attr]
         # start checking from 1st value because splitting at 0th index will return the original array
         for row in range (1, len(sortedArr)):
-            # classChanged = False
-            # still need to skip some split points if useless (cf specs)    
             splitPoint = sortedArr[row][attr]
             currClass = sortedArr[row][0] # because first attr contains labels
             if ((prevSplitPoint != splitPoint)):
@@ -61,10 +73,7 @@ def findBestSplitPoint(data):
                 if currIG > bestIG:
                     bestIG = currIG
                     bestSplitPoint = [attr, splitPoint]
-                # print(splitPoint, currClass)
             prevSplitPoint = splitPoint
-            # prevClass = currClass
-
 
     return bestSplitPoint
 
--- node.py
+++ node.py
@@ -2,18 +2,18 @@ from question1 import readFile
 from question1 import countFrequency
 import math
 
-def calcEntropy(labels):
-    freq = countFrequency(labels)
-    total = 0
-    for item in freq:
-        total += freq[item] 
+# def calcEntropy(labels):
+#     freq = countFrequency(labels)
+#     total = 0
+#     for item in freq:
+#         total += freq[item] 
 
-    entropy = 0
+#     entropy = 0
 
-    for item in freq:
-        entropy -= (freq[item] / total) * math.log(freq[item] / total, 2)
+#     for item in freq:
+#         entropy -= (freq[item] / total) * math.log(freq[item] / total, 2)
 
-    return entropy
+#     return entropy
 
 class Node:
     def __init__(self):
@@ -55,24 +55,24 @@ class DecisionNode(Node):
 
 
 
-    def calcIG(self, subLabel1, subLabel2):
-        dataCount = len(subLabel1) + len(subLabel2)
-        childEntropy = (len(subLabel1) / dataCount) * calcEntropy(subLabel1) + (len(subLabel2) / dataCount) * calcEntropy(subLabel2)
-        return self.entropy - childEntropy
+    # def calcIG(self, subLabel1, subLabel2):
+    #     dataCount = len(subLabel1) + len(subLabel2)
+    #     childEntropy = (len(subLabel1) / dataCount) * calcEntropy(subLabel1) + (len(subLabel2) / dataCount) * calcEntropy(subLabel2)
+    #     return self.entropy - childEntropy
 
 
 
 
         
-attributes, labels = readFile("data/toy.txt")
+# attributes, labels = readFile("data/toy.txt")
 
-root = DecisionNode(attributes, labels)
-print("Root has entropy: " + str(root.entropy))
+# root = DecisionNode(attributes, labels)
+# print("Root has entropy: " + str(root.entropy))
 
-attributes1, labels1 = readFile("data/toy_sub1.txt")
-attributes2, labels2 = readFile("data/toy_sub2.txt")
-test = root.calcIG(labels1, labels2)
-print(test)
+# attributes1, labels1 = readFile("data/toy_sub1.txt")
+# attributes2, labels2 = readFile("data/toy_sub2.txt")
+# test = root.calcIG(labels1, labels2)
+# print(test)
 
 
 
