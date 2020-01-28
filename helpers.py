4c90fc8e83d5f6dcb0edbf753727ae571fedef78
Zijian_Ou <kenou1996@126.com>
2020-01-28 16:13:22 +0000

    temporary save

--- helpers.py
+++ helpers.py
@@ -1,11 +1,33 @@
-function sortCategory():
-function sortLabel():
+import numpy as np
+import SplitInfo as si
+
+# stud functions
+def sortByColAndLabel(data, col):
+    return np.array([[1, 2, 3], 
+                     [4, 5, 6],
+                     [7, 8, 9]])
+
+def findBestSplitPoint(dataset):
+    splitInfo = si.SplitInfo(1, 1)
+    splitIndex = 2
+    return splitInfo, splitIndex
 
 
-function entropy(subset):
-    return entropy
+# @param dataset NxAttr array
+# @param splitInfo = [splitAttribute, splitPoint]
+# @retrun an splitPoint x Attr array and an (N - splitPoint) x Attr array
+def split(dataset, splitInfo):
+    trueData = np.array([])
+    falseData = np.array([])
 
-function IG(base, subset1, subset2):
-    return IG
+    for data in dataset:
+        if (data[0] > splitInfo.value):
+            
 
-function getLabel(ascii):
+
+# Tests
+# dataset = np.array([[1, 2, 3],
+#                     [4, 5, 6],
+#                     [7, 8, 9]])
+# splitInfo = [2, 2]
+# print(split(dataset, splitInfo))
