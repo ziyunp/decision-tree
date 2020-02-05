3c679df6053935095a4a7da15aa700a781ac08db
zo19 <zo19@ic.ac.uk>
2020-02-05 20:29:24 +0000

    feat: add a max_depth test

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from helpers import *
 from classification import *
 from eval import *
 from cross_validation import *
+from copy import *
+from prune import *
 # import json
 
 ### Hardcoded data ###
@@ -11,14 +13,20 @@ from cross_validation import *
 # jsonData["root"] = []
 ######################
 
+# read datasets
 train_attributes, train_labels = read_file("data/train_full.txt")
+test_attributes, test_labels = read_file("data/test.txt")
+val_attributes, val_labels = hp.read_file("data/validation.txt")
+
+# train
 dtClassifier = DecisionTreeClassifier()
 dtClassifier.train(train_attributes, train_labels)
 
-test_attributes, test_labels = read_file("data/test.txt")
+# predict
 predictions = dtClassifier.predict(test_attributes)
 predictions_cross_validation = cross_validation("data/train_full.txt", 10).predict(test_attributes)
 
+# evaluate
 evaluator = Evaluator()
 confusion = evaluator.confusion_matrix(predictions, test_labels)
 confusion_cross_validation = evaluator.confusion_matrix(predictions_cross_validation, test_labels)
@@ -27,3 +35,15 @@ print("Accuracy when training with cross-validation: ", evaluator.accuracy(confu
 
 # with open('save.json', 'w') as outfile:
 #     json.dump(json_data, outfile)
+
+
+def max_depth_test(dt_classifier, val_attributes, val_labels):
+    """
+    print out the depth of the given tree and the depth after pruning
+    """
+    dt_classifier_copy = copy(dt_classifier)
+    print("Before pruning the maximum depth of tree is: ", dt_classifier_copy.tree.get_depth(0))
+    prune(dt_classifier_copy, dt_classifier_copy.tree, val_attributes, val_labels)
+    print("After pruning the maximum depth of tree is: ", dt_classifier_copy.tree.get_depth(0))
+
+max_depth_test(dtClassifier, val_attributes, val_labels)
