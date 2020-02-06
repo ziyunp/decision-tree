cf8fa23239087fb3fdf65def23b687d07a86640c
Zijian_Ou <kenou1996@126.com>
2020-02-06 00:41:40 +0000

    feat: ZO a more agressive pruning

--- prune.py
+++ prune.py
@@ -7,6 +7,59 @@ import classification as cf
 import eval as ev
 import copy as cp
 
+def prune_zo(dt_classifier, node, validation, annotation, prev_node = None, node_class = None):
+    if isinstance(node, nd.Leaf_node):
+        return node
+    else:
+        if isinstance(node.child_true, nd.Decision_node):
+            node.child_true = prune_zo(dt_classifier, node.child_true, validation, annotation, node, True)
+        if isinstance(node.child_false, nd.Decision_node):
+            node.child_false = prune_zo(dt_classifier, node.child_false, validation, annotation, node, False)
+
+        # save the current node
+        node_backup = cp.deepcopy(node)   
+        
+        # get original accuracy
+        base_prediction = dt_classifier.predict(validation)
+        evaluator = ev.Evaluator()
+        base_confusion = evaluator.confusion_matrix(base_prediction, annotation)
+        base_accuracy = evaluator.accuracy(base_confusion)
+
+        # prune chidlren
+        if node_class == None: # root_node
+            return node
+        cur_freq, init_freq = remove_children(node)
+        node = nd.Leaf_node(cur_freq, init_freq)
+        if node_class == True:
+            prev_node.child_true = node
+        elif node_class == False:
+            prev_node.child_false = node
+
+        # get new accuracy
+        cur_prediction = dt_classifier.predict(validation)
+        cur_confusion = evaluator.confusion_matrix(cur_prediction, annotation)
+        cur_accuracy = evaluator.accuracy(cur_confusion)
+
+        # decide whether confirm pruning
+        if cur_accuracy >= base_accuracy:
+            print("The accuracy improves from {} to {}".format(base_accuracy, cur_accuracy))
+            return node
+        else:
+            return node_backup
+
+def remove_children(node):
+    if (isinstance(node.child_true, nd.Leaf_node)):
+        freq_true = node.child_true.predictions
+        init_freq = node.child_true.init_freq
+    else:
+        freq_true, init_freq = remove_children(node.child_true)
+    if (isinstance(node.child_false, nd.Leaf_node)):
+        freq_false = node.child_false.predictions
+    else:
+        freq_false, _ = remove_children(node.child_false)
+    return hp.merge_freq(freq_true, freq_false), init_freq
+
+
 def prune(tree, node, validation, annotation, prev_node = None, left = None):
 
     if isinstance(node, nd.Leaf_node):
