cd4fc04f9d386e755eddf20ee528603583542908
Zijian_Ou <kenou1996@126.com>
2020-02-07 11:31:26 +0000

    feat: Add tests for combing cross validation and pruning

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from prune import *
 ######################
 
 # read datasets
-train_attributes, train_labels = read_file("data/train_full.txt")
+train_attributes, train_labels = read_file("data/train_noisy.txt")
 test_attributes, test_labels = read_file("data/test.txt")
 val_attributes, val_labels = hp.read_file("data/validation.txt")
 
@@ -37,17 +37,47 @@ def max_depth_test(dt_classifier, val_attributes, val_labels):
     """
     print out the depth of the given tree and the depth after pruning
     """
-    dt_classifier_copy = copy(dt_classifier)
+    dt_classifier_copy = deepcopy(dt_classifier)
     print("Before pruning the maximum depth of tree is: ", dt_classifier_copy.tree.get_depth(0))
-    prune(dt_classifier_copy, dt_classifier_copy.tree, val_attributes, val_labels)
+    prune_more(dt_classifier_copy, dt_classifier_copy.tree, val_attributes, val_labels)
     print("After pruning the maximum depth of tree is: ", dt_classifier_copy.tree.get_depth(0))
 
-def cross_validation_accuracy(test_attributes, evaluator):
-    predictions_cross_validation = cross_validation("data/train_full.txt", 10).predict(test_attributes)
-    confusion_cross_validation = evaluator.confusion_matrix(predictions_cross_validation, test_labels)
-    print("Accuracy when training with cross-validation: ", evaluator.accuracy(confusion_cross_validation))
+# def cross_validation_accuracy(test_attributes, evaluator):
+#     predictions_cross_validation = cross_validation("data/train_full.txt", 10, None).predict(test_attributes)
+#     confusion_cross_validation = evaluator.confusion_matrix(predictions_cross_validation, test_labels)
+#     print("Accuracy when training with cross-validation: ", evaluator.accuracy(confusion_cross_validation))
 
-# cross_validation("data/train_full.txt", 10, True)
+def cross_validation_accuracy_after_pruning_more():
+    cross_validation("data/train_full.txt", 10, True, "prune_more")
+
+def cross_validation_accuracy_after_pruning():
+    cross_validation("data/train_full.txt", 10, True, "prune")
+
+def cross_validation_accuracy():
+    cross_validation("data/train_full.txt", 10, True, None)
+
+def accuracy_with_prune_more(dt_classifier, evaluator, val_attributes, val_labels):
+    dt_classifier_copy = deepcopy(dt_classifier)
+    dt_classifier_copy.set_max_share_hyperparameter(0.004)
+    prune_more(dt_classifier_copy, dt_classifier_copy.tree, val_attributes, val_labels)
+    predictions = dt_classifier_copy.predict(val_attributes)
+    confusion = evaluator.confusion_matrix(predictions, val_labels)
+    print("Accuracy on validation after dataset prune_more is: ", evaluator.accuracy(confusion))
+
+def accuracy_with_prune(dt_classifier, evaluator, val_attributes, val_labels):
+    dt_classifier_copy = deepcopy(dt_classifier)
+    dt_classifier_copy.set_max_share_hyperparameter(0.004)
+    prune(dt_classifier_copy, dt_classifier_copy.tree, val_attributes, val_labels)
+    predictions = dt_classifier_copy.predict(val_attributes)
+    confusion = evaluator.confusion_matrix(predictions, val_labels)
+    print("Accuracy on validation after dataset prune is: ", evaluator.accuracy(confusion))
+
+def accuracy_no_prune(dt_classifier, evaluator, val_attributes, val_labels):
+    dt_classifier_copy = deepcopy(dt_classifier)
+    dt_classifier_copy.set_max_share_hyperparameter(0.004)
+    predictions = dt_classifier_copy.predict(val_attributes)
+    confusion = evaluator.confusion_matrix(predictions, val_labels)
+    print("Accuracy on validation dataset is: ", evaluator.accuracy(confusion))
 
 #################################################################
 
@@ -56,3 +86,18 @@ def cross_validation_accuracy(test_attributes, evaluator):
 
 # test2
 # cross_validation_accuracy(test_attributes, evaluator)
+
+# test3
+# cross_validation_accuracy_after_pruning_more()
+# cross_validation_accuracy_after_pruning()
+# cross_validation_accuracy()
+
+# test4
+# accuracy_no_prune(dtClassifier, evaluator, val_attributes, val_labels)
+# accuracy_with_prune(dtClassifier, evaluator, val_attributes, val_labels)
+# accuracy_with_prune_more(dtClassifier, evaluator, val_attributes, val_labels)
+
+# test5
+# accuracy_no_prune(dtClassifier, evaluator, test_attributes, test_labels)
+# accuracy_with_prune(dtClassifier, evaluator, test_attributes, test_labels)
+# accuracy_with_prune_more(dtClassifier, evaluator, test_attributes, test_labels)
