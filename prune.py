4e36f7e3539115d359edae343573332ff89a35d8
Zijian_Ou <kenou1996@126.com>
2020-02-08 19:40:11 +0000

    refac: updated the prune_to_max_depth() to current naming system

--- prune.py
+++ prune.py
@@ -20,8 +20,8 @@ def pruning_helper(node, max_depth):
         # else: # a Leaf Node, no need to change
     else: # a Decision Node, continue to ge deeper
         if (not isinstance(node, nd.Leaf_node)):
-            node.childTrue = pruning_helper(node.childTrue, max_depth - 1)
-            node.childFalse = pruning_helper(node.childFalse, max_depth - 1)
+            node.child_true = pruning_helper(node.child_true, max_depth - 1)
+            node.child_false = pruning_helper(node.child_false, max_depth - 1)
     return node
 
 
