dc14c1b96c1273e8e1a884ee697f2cc37a33a3dc
Ziyun Poh <zp619@texel06.doc.ic.ac.uk>
2020-02-09 18:19:38 +0000

    add comments, use get_test_accuracy instead

--- cross_validation.py
+++ cross_validation.py
@@ -5,8 +5,9 @@ from eval import *
 from prune import *
 
 HYPERPARAMETER_THRESHOLD = 0.01
-def split_dataset(dataset, k):
+STEP = 0.002
 
+def split_dataset(dataset, k):
     subsets = []
     divider = len(dataset) // k
     for i in range (k):
@@ -14,13 +15,14 @@ def split_dataset(dataset, k):
 
     return np.array(subsets)
 
-
+# This function allows optional hyperparameter tuning and pruning 
 def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = None):
 
     attributes, labels = read_file(filename)
     dataset = get_data(attributes, labels)
     length = len(dataset)
 
+    # check if k value is valid
     min_k = 2
     if hyperparameter_tuning:
         min_k = 3
@@ -28,9 +30,9 @@ def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = No
         print("Error: invalid k value")
         return
 
-    # Shuffle
+    # shuffle dataset
     np.random.shuffle(dataset)
-
+    # split dataset
     subsets = split_dataset(dataset, k)
     models_list = [] 
     all_trees = []
@@ -38,9 +40,15 @@ def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = No
     for i in range (k - 1):
         accuracy_list = [] 
         training_dataset = []
+        test_dataset = []
+
+        # only need a validation dataset if hyperparameter_tuning or pruning
         if hyperparameter_tuning or not prune_func == None:
             validation_dataset = []
-        test_dataset = []
+
+        # divide subsets into test, validation and training sets
+        # ratio of test:validation:training is 1:1:k-2 if hyperparameter_tuning
+        # otherwise ratio is 1:k-1
         test_dataset = subsets[i]
         for training_subset in np.delete(subsets, i, 0):
             if (hyperparameter_tuning or not prune_func == None) and len(validation_dataset) == 0:
@@ -50,18 +58,18 @@ def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = No
             else:
                 training_dataset = np.append(training_dataset, training_subset, axis=0)
         
-
         if hyperparameter_tuning: 
-            # Trying different hyperparameters
-            for max_share in np.arange (0.00, HYPERPARAMETER_THRESHOLD, 0.002):
+            # Try hyperparameters from 0 to HYPERPARAMETER_THRESHOLD
+            # at an interval of STEP
+            for max_share in np.arange (0.00, HYPERPARAMETER_THRESHOLD, STEP):
                 accuracy, tree = run(training_dataset, validation_dataset, max_share, prune_func)
                 accuracy_list.append([max_share, accuracy, tree])
 
             best_max_share = accuracy_list[0][0]
             best_accuracy = accuracy_list[0][1]
             best_tree = accuracy_list[0][2]
-
-            # Finding the best accuracy
+                        
+            # Find best hyperparameter by the accuracy of predictions on the validation set
             for j in range (1, len(accuracy_list)):
                 if (accuracy_list[j][1] > best_accuracy):
                     best_accuracy = accuracy_list[j][1]
@@ -71,24 +79,23 @@ def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = No
             print("Best max_share hyperparameter is: ", best_max_share)
             print("Best accuracy is: ", best_accuracy)
 
-            # accuracy_test, tree = run(training_dataset, test_dataset, best_max_share, None)
-            x_test, y_test = test_dataset[:,:-1], [chr(test_dataset[i][-1]) for i in range(len(test_dataset))]
-            accuracy_test = get_accuracy(best_tree, x_test, y_test)
+            accuracy_test = get_test_accuracy(best_tree, test_dataset)
+            
             print("Accuracy on the test dataset with the best max_share is: ", accuracy_test)
 
             all_trees.append(best_tree)
             models_list.append([accuracy_test, best_tree])
 
-            print("======================")
-        # total = np.nansum([1 - acc for acc in accuracy])
-        
-        elif (not prune_func == None): # no hyperparameter tuning but with prunning
+        # no hyperparameter tuning but with pruning
+        elif (not prune_func == None): 
             accuracy_val, tree = run(training_dataset, validation_dataset, 0, prune_func)
             print("With pruning: ", prune_func, "the accuracy on validation set is: ", accuracy_val)
-            x_test, y_test = test_dataset[:,:-1], [chr(test_dataset[i][-1]) for i in range(len(test_dataset))]
-            accuracy_test = get_accuracy(tree, x_test, y_test)
-            models_list.append([accuracy_test, tree])
+            accuracy_test = get_test_accuracy(tree, test_dataset)
 
+            all_trees.append(tree)
+            models_list.append([accuracy_test, tree])
+        
+        # train without hyperparameter tuning nor pruning
         else:
             accuracy_test, tree = run(training_dataset, test_dataset, 0, None)
             all_trees.append(tree)
@@ -96,13 +103,11 @@ def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = No
 
     best_accuracy = models_list[0][0]
     best_tree = models_list[0][1]
-    # Finding the best accuracy
+    # Find the best accuracy
     for j in range (1, len(models_list)):
         if (models_list[j][0] > best_accuracy):
             best_accuracy = models_list[j][0]
             best_tree = models_list[j][1]
-    # print("The best accuracy on test dataset is: ", best_accuracy)
-    # No use
 
     average_accuracy = np.average([models_list[i][0] for i in range (len(models_list))])
     standard_deviation = np.std([models_list[i][0] for i in range (len(models_list))])
@@ -110,10 +115,7 @@ def cross_validation(filename, k, hyperparameter_tuning = False, prune_func = No
 
     return best_tree, all_trees
 
-
-
 def run(training_dataset, test_dataset, max_share, prune_func):
-
     x,y = training_dataset[:,:-1], [chr(training_dataset[i][-1]) for i in range(len(training_dataset))]
     x_test, y_test = test_dataset[:,:-1], [chr(test_dataset[i][-1]) for i in range(len(test_dataset))]
 
@@ -121,27 +123,19 @@ def run(training_dataset, test_dataset, max_share, prune_func):
     classifier.set_max_share_hyperparameter(max_share)
     classifier = classifier.train(x, y)
 
-    # print("Pruning the tree...")
     if (prune_func == "prune"):
         prune(classifier, classifier.tree, x_test, y_test)
     elif (prune_func == "prune_more"):
         prune_more(classifier, classifier.tree, x_test, y_test)
 
-    accuracy = get_accuracy(classifier, x_test, y_test)
+    accuracy = get_test_accuracy(classifier,test_dataset)
 
     return accuracy, classifier
 
-
-def get_accuracy(classifier, x_test, y_test):
-    # print("Testing the decision tree...")
+def get_test_accuracy(classifier, test_dataset):
+    x_test, y_test = test_dataset[:,:-1], [chr(test_dataset[i][-1]) for i in range(len(test_dataset))]
     predictions = classifier.predict(x_test)
-
     evaluator = Evaluator()
-    
-    # requires str in both params
     confusion = evaluator.confusion_matrix(predictions, y_test)
-    
-    # print("Confusion matrix:")
-    # print(confusion)
-
     return evaluator.accuracy(confusion)
+
